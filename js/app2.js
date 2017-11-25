e216cf94e4b6ad48ff29da14d8ac544673fc135e
shorqmar <shoroqmar@yahoo.com>
2017-11-25 15:05:53 +0200

    Almost

--- js/app2.js
+++ js/app2.js
@@ -1,128 +1,106 @@
-
 var img1 = document.createElement("img");
-img1.src = "https://cdn0.iconfinder.com/data/icons/Aristocracy_WebDesignTuts/48/Marker.png";
-      var markersList = [];
+img1.src =
+  "https://cdn0.iconfinder.com/data/icons/Aristocracy_WebDesignTuts/48/Marker.png";
+var markersList = [];
 
-   var locations=[
-              {title: 'Kaybayen Market', location: {lat : 25.403643,lng :49.578320}},
-              {title: 'Car Washing', location: {lat : 25.403413,lng : 49.579538}},
-              {title: 'Upick', location: {lat : 25.402083,lng :  49.578202}},
-              {title: 'StarBucks', location: {lat : 25.400603, lng :  49.577064}},
-              {title: 'Riyadh Bank ATM', location: {lat : 25.398170, lng : 49.580054}},
-        ];
+var locations = [
+  { title: "Kaybayen Market", location: { lat: 25.403643, lng: 49.57832 } },
+  { title: "Car Washing", location: { lat: 25.403413, lng: 49.579538 } },
+  { title: "Upick", location: { lat: 25.402083, lng: 49.578202 } },
+  { title: "StarBucks", location: { lat: 25.400603, lng: 49.577064 } },
+  { title: "Riyadh Bank ATM", location: { lat: 25.39817, lng: 49.580054 } }
+];
 function initMap() {
-        var myLatLng = {lat: 25.403551, lng: 49.578319};
+  var myLatLng = { lat: 25.403551, lng: 49.578319 };
 
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 18,
-          center: myLatLng
-        
-        });
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 18,
+    center: myLatLng
+  });
 
-var Info = new google.maps.InfoWindow();
-        var bounds = new google.maps.LatLngBounds();
-    
-for (var i = 0; i < locations.length; i++) {
-          var position = locations[i].location;
-          var title = locations[i].title;
-            
-          var marker = new google.maps.Marker({
-            map: map,
-            position: position,
-            title: title,
-            animation: google.maps.Animation.DROP,
-            id: i,
-            draggable: true,
-            icon:"https://cdn4.iconfinder.com/data/icons/evil-icons-user-interface/64/location-48.png"
+  var Info = new google.maps.InfoWindow();
+  var bounds = new google.maps.LatLngBounds();
 
-          });
-    marker.addListener('click', toggleBounce);
+  for (var i = 0; i < locations.length; i++) {
+    var position = locations[i].location;
+    var title = locations[i].title;
 
+    var marker = new google.maps.Marker({
+      map: map,
+      position: position,
+      title: title,
+      animation: google.maps.Animation.DROP,
+      id: i,
+      draggable: true,
+      icon:
+        "https://cdn4.iconfinder.com/data/icons/evil-icons-user-interface/64/location-48.png"
+    });
+    locations[i].marker = marker;
+    marker.addListener("click", toggleBounce);
 
-          markersList.push(marker);
-          marker.addListener('click', function() {
-            FillInfoWindow(this, Info);
-          });
-            
-            
-          bounds.extend(markersList[i].position);
-            function toggleBounce() {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-  } else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-  }
-}
-              google.maps.event.addListener(marker, 'click', function() {      
-      
-      marker.setAnimation(google.maps.Animation.BOUNCE);
-      setTimeout(function(){marker.setAnimation(null);
-    }, 1450);
-    
+    markersList.push(marker);
+    marker.addListener("click", function() {
+      FillInfoWindow(this, Info);
     });
 
+    bounds.extend(markersList[i].position);
+    function toggleBounce() {
+      if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+      } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
       }
-        
-        map.fitBounds(bounds);
-    
-      }
+    }
+    google.maps.event.addListener(marker, "click", function() {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+      setTimeout(function() {
+        marker.setAnimation(null);
+      }, 1450);
+    });
+  }
 
-      function FillInfoWindow(marker, infowindow) {
-        if (infowindow.marker != marker) {
-          infowindow.marker = marker;
-          infowindow.setContent('<div>' + marker.title + '</div>');
-          infowindow.open(map, marker);
-          infowindow.addListener('closeclick',function(){
-            infowindow.setMarker = null;
-          });
-        }
- 
-
-      } //initiat 
- 
-
-function viewModel() { 
-this.search = ko.observable("");
+  map.fitBounds(bounds);
 }
 
+function FillInfoWindow(marker, infowindow) {
+  if (infowindow.marker != marker) {
+    infowindow.marker = marker;
+    infowindow.setContent("<div>" + marker.title + "</div>");
+    infowindow.open(map, marker);
+    infowindow.addListener("closeclick", function() {
+      infowindow.setMarker = null;
+    });
+  }
+} //initiat
 
-var myViewModel = new viewModel() ; 
+function ViewModel() {
+  var self = this;
+  this.search = ko.observable("");
+  this.filter = ko.computed(function() {
+    var filteredLocations = [];
+    var searchTerm = self.search().toLowerCase();
+    console.log(searchTerm);
+
+    for (var i = 0; i < locations.length; i++) {
+      var title = locations[i].title.toLowerCase();
+      var includes = title.includes(searchTerm); // true or false
+
+      console.log(title, searchTerm, includes);
+
+      if (includes) filteredLocations.push(locations[i]);
+
+      if (locations[i].marker) {locations[i].marker.setVisible(includes); // true or false
+                                locations[i].marker.setAnimation(google.maps.Animation.BOUNCE);
+                                
+                               }
+        
+    }
+
+    return filteredLocations;
+  });
+}
+
+var myViewModel = new ViewModel();
 ko.applyBindings(myViewModel);
-
-myViewModel.search.subscribe(function(newValue){
-    var searchTerm = newValue.toLowerCase();
-    filterMapsBasedOnText(searchTerm);
-    
-});
-
-var theNewMarkerPos ; 
-var theNewMarkerTitle;
-
-function filterMapsBasedOnText(search){ 
-    
-    ko.utils.arrayFirst(self.locations, function(search) {
-        
-        for ( var i =0 ; i<locations.length;i++){
-    if (locations[i].title.toLowerCase().includes(search)){
-        theNewMarkerPos = self.locations[i].location; 
-        theNewMarkerTitle = self.locations[i].title;
-            
-          var marker = new google.maps.Marker({
-            setMap: map,
-            position: theNewMarkerPos,
-            title: theNewMarkerTitle,
-            animation: google.maps.Animation.BOUNCE,
-            id: i,
-            draggable: true
-          });
-    }//if statment
-        
-}//for loop
-    }//array match function
-                       );// array match 
-    
-    
-    
-        } //filter function
 
 
